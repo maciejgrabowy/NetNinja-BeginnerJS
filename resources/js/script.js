95b42039343c94507a4e08b3be21af7b754f84c2
Maciej Grabowy <macgrabowy@gmail.com>
2019-09-30 21:13:51 +0200

    add more if statements

--- resources/js/script.js
+++ resources/js/script.js
@@ -9,7 +9,24 @@ numbersNumbers += 1;
 console.log(numbersNumbers);
 
 //trying a loop or two
+if (numbersNumbers == 16) {
+  console.log("Yup, it's 16")
+} else {
+  console.log("Nope, it's not 16");
+}
 
-if (numberNumbers = 16) {
-  console.log("Yup, it's 16");
+//age tester
+var myAge = 33;
+
+if (myAge > 30) {
+  console.log("You're over thirty! Basically a boomer!");
+
+} else if (myAge > 20) {
+  console.log("You're over twenty! Shine on!");
+
+} else if (myAge > 10) {
+  console.log("You're over ten! Good job!");
+
+} else {
+  console.log("You're basically a baby");
 }
