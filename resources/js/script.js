82b88eb61be5efe9c5aa669641b0b384d4d26788
Maciej Grabowy <macgrabowy@gmail.com>
2019-09-30 19:50:04 +0200

    testing basic console logging, loops

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,15 @@
+//console logging stuff 
 console.log(4);
+console.log("batonik");
+var theNumberTen = 10;
+var numbersNumbers = theNumberTen + 5;
+document.write(numbersNumbers);
+console.log(numbersNumbers);
+numbersNumbers += 1;
+console.log(numbersNumbers);
+
+//trying a loop or two
+
+if (numberNumbers = 16) {
+  console.log("Yup, it's 16");
+}
