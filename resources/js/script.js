d611eb5bd47d0f170aeb56130d9d7373eb98e2d0
Maciej Grabowy <macgrabowy@gmail.com>
2019-10-03 18:23:38 +0200

    added "for" loop

--- resources/js/script.js
+++ resources/js/script.js
@@ -8,7 +8,7 @@ console.log(numbersNumbers);
 numbersNumbers += 1;
 console.log(numbersNumbers);
 
-//trying a loop or two
+//trying an if statement or two
 if (numbersNumbers == 16) {
   console.log("Yup, it's 16")
 } else {
@@ -17,7 +17,6 @@ if (numbersNumbers == 16) {
 
 //age tester
 var myAge = 33;
-
 if (myAge > 30) {
   console.log("You're over thirty! Basically a boomer!");
 
@@ -30,3 +29,12 @@ if (myAge > 30) {
 } else {
   console.log("You're basically a baby");
 }
+
+var links = document.getElementsByTagName("a");
+
+for (i = 1; i < links.length; i++) {
+  console.log("that's a link number" + 1);
+  
+}
+
+document.write("all links are now looped, friend");
