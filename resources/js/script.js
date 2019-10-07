bf871d2f99307a7b2c713295b0d91afcc7ad72a2
Maciej Grabowy <macgrabowy@gmail.com>
2019-10-07 18:46:04 +0200

    Added the first objects

--- resources/js/script.js
+++ resources/js/script.js
@@ -52,3 +52,40 @@ console.log("The length is " + myString.length);
 console.log(myString.toUpperCase());
 console.log(myString.toLowerCase());
 console.log("The index of the word 'root' is " + myString.indexOf("root"));
+
+//arrays
+myArray = [];
+myArray[0] = 10;
+myArray[1] = "Derp"
+myArray[2] = 12.5;
+myArray[3] = true;
+console.log(myArray);
+
+myArray2 = [2,5,3,true,"stuff"];  //this is much faster
+console.log(myArray2);
+myArray2.sort();
+console.log("Sorted: " + myArray2);
+myArray2.reverse();
+console.log(myArray2);
+
+//objects
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Mac";
+myCar.drive = function() {
+  console.log("I'm driving now");
+};
+
+myCar.drive();
+
+var myCar2 = {
+
+  maxSpeed: 70,
+  driver: "The other guy",
+  drive: function(speed, time) {
+    console.log(speed * time);    
+  }
+};
+
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
